41be879490cebbadf5067be8d56534bef19dc1da
Manisha <er.manisha08@gmail.com>
2023-10-15 23:26:23 +0530

    Creating server side app

---
+++ Desktop/Project/GoodFood_Server/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      babel: {
+        dangerouslyAddModulePathsToTranspile: ["@ui-kitten/components"],
+      },
+    },
+    argv
+  );
+  return config;
+};
